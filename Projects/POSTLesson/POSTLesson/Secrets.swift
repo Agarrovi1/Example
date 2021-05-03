22bf129b35d5d6c565acc652a44705719170039e
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2021-05-03 09:42:25 -0400

    Add files via upload

---
+++ Projects/POSTLesson/POSTLesson/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  POSTLesson
+//
+//  Created by Angela Garrovillas on 9/10/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+struct Secrets {
+    static let APIKey = "key4cDCgU2BDU5kqz"
+}
