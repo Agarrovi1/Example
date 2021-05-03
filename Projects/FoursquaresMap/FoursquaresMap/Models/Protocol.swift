6d5a0994ecc259cf7a31afe1020c82649a5e3f11
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2021-05-03 09:23:45 -0400

    Add files via upload

---
+++ Projects/FoursquaresMap/FoursquaresMap/Models/Protocol.swift
@@ -0,0 +1,14 @@
+//
+//  Protocol.swift
+//  FoursquaresMap
+//
+//  Created by Angela Garrovillas on 11/8/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+protocol Reload {
+    func reloadCollectionView()
+}
+
+
