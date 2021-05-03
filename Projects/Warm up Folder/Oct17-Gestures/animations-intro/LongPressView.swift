eb73544c08164800804a43c16b2ea3f0f8cb7bcf
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2021-05-03 10:22:51 -0400

    Add files via upload

---
+++ Projects/Warm up Folder/Oct17-Gestures/animations-intro/LongPressView.swift
@@ -0,0 +1,20 @@
+//
+//  LongPressView.swift
+//  animations-intro
+//
+//  Created by Angela Garrovillas on 10/17/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class LongPressView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ Projects/Warm up Folder/Oct17-Gestures/animations-intro/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  animations-intro
+//
+//  Created by David Rifkin on 10/11/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var tappedScreenRecognizer: UILongPressGestureRecognizer = {
+        let tapGesture = UILongPressGestureRecognizer(target: self, action: #selector(tapGestureTapped))
+        
+        return tapGesture
+    }()
+    
+    lazy var bounceButton: UIButton = { [unowned self] in
+        let button = UIButton()
+        button.setTitle("Drop", for: .normal)
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(bounceButtonPressed), for: .touchUpInside)
+        button.layer.cornerRadius =  button.frame.width / 2
+        return button
+    }()
+    
+    lazy var myView: UIView = { [unowned self] in
+        let view = UIView()
+        view.backgroundColor = .green
+        view.layer.cornerRadius = view.frame.height / 2
+        return view
+    }()
+    
+    
+    lazy var myViewHeightConstraint: NSLayoutConstraint = {
+        myView.heightAnchor.constraint(equalToConstant: 200)
+    }()
+
+    lazy var myViewWidthConstraint: NSLayoutConstraint = {
+        myView.widthAnchor.constraint(equalToConstant: 200)
+    }()
+
+    lazy var myViewCenterXConstraint: NSLayoutConstraint = {
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    }()
+
+    lazy var myViewTopAnchorConstraint: NSLayoutConstraint = {
+        myView.topAnchor.constraint(equalTo: view.topAnchor, constant: myViewHeightConstraint.constant)
+    }()
+    
+    @objc func bounceButtonPressed(sender: UIButton) {
+        let oldConstant = self.myViewTopAnchorConstraint.constant
+        self.myViewTopAnchorConstraint.constant =
+            self.view.bounds.maxY - myViewHeightConstraint.constant
+        myView.alpha = 1.0
+        //use constraints instead of number mathy things
+        
+        UIView.animate(withDuration: 1.2, delay: 0.4, options: [.autoreverse], animations: {
+            self.view.layoutIfNeeded()
+            self.myView.alpha = 0.0
+        }, completion: { (done) in
+            self.myView.alpha = 1.0
+            self.myViewTopAnchorConstraint.constant = oldConstant
+        })
+    }
+    
+    @objc private func tapGestureTapped() {
+        print("tapped")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        self.view.addSubview(myView)
+        self.view.addSubview(bounceButton)
+        configureConstraints()
+        self.myView.addGestureRecognizer(tappedScreenRecognizer)
+    }
+    
+    private func configureConstraints() {
+        configureMyViewConstraints()
+        constrainDropButton()
+    }
+    
+    private func configureMyViewConstraints() {
+        myView.translatesAutoresizingMaskIntoConstraints = false
+           NSLayoutConstraint.activate([
+                myViewHeightConstraint,
+                myViewWidthConstraint,
+                myViewCenterXConstraint,
+                myViewTopAnchorConstraint
+           ])
+    }
+    
+    private func constrainDropButton() {
+        bounceButton.translatesAutoresizingMaskIntoConstraints = false
+       
+        NSLayoutConstraint.activate([
+            bounceButton.heightAnchor.constraint(equalToConstant: 50),
+            bounceButton.widthAnchor.constraint(equalToConstant: 50),
+            bounceButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            bounceButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -300)
+            ])
+    }
+    
+    @objc private func respondToTap() {
+        print("tapping")
+
+    }
+}
